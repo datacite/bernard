271196f6b7179a11127a2f3cbc539c5eb31a8f27
Richard Hallett <richard@richardhallett.com>
2018-03-14 18:02:11 +0000

    Lambda sets up basicConfig first so getLogger instead

--- lambdas/check_links/check_links_runner.py
+++ lambdas/check_links/check_links_runner.py
@@ -21,7 +21,8 @@ REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
 REDIS_PORT = os.getenv('REDIS_PORT', 6379)
 
 # Configure basic logging
-logging.basicConfig(level=LOG_LEVEL)
+logger = logging.getLogger()
+logger.setLevel(level=LOG_LEVEL)
 
 def get_samples(sample_per_client=1):
     """Get random samples of doi's across clients"""
@@ -60,7 +61,7 @@ def get_samples(sample_per_client=1):
 def seed_pid(pid, url):
     """Push a pid url into redis for processing by the crawler"""
     pl = {'pid': pid, 'url': url}
-    logging.info("Queueing '{0}' with url '{1}' for processing".format(pid, url))
+    logger.info("Queueing '{0}' with url '{1}' for processing".format(pid, url))
     # Redis server connector
     redis_conn = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=0)
     redis_conn.lpush(START_URLS_KEY, json.dumps(pl))
@@ -70,7 +71,7 @@ def lambda_handler(event, context):
 
     # Obtain the random samples for checking
     num_samples = 1
-    logging.info("Sampling for %s samples per client" % (num_samples))
+    logger.info("Sampling doi's for %s samples per client" % (num_samples))
     samples = get_samples(num_samples)
 
     # Parse samples and request for checking each one.
